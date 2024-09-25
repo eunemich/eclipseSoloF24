f8e592aa2d438883681c59e0ecf4be6508cfcc2f
gloobywooby <notthebees777@gmail.com>
2024-09-25 14:47:19 -0400

    "Programmer 2 added a print statement"

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -3,6 +3,7 @@ package eclipseSoloProjA;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hi");
-		System.out.println("by");
+		System.out.println("bye");
+		System.out.println("Programmer 2 did this");
 	}
 }
