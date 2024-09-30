2a101f2a62114211762ecfa5f6449319dc0d1c2f
gloobywooby <notthebees777@gmail.com>
2024-09-29 21:41:15 -0400

    altered println message

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("bye");
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this Programmer 1 did this");
 	}
 }
